63d8b38725b2d8c753cc2860e13bea0170edb7d3
Primordial-Devv <primordial.devv@gmail.com>
2026-09-16 20:54:12 +0200

    CHORE - Version 1.0.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Siku Studio'
 description 'A modern, immersive radio system for the SIKU ecosystem — featuring a realistic in-device interface, channel management, player communication, and a clean, modular architecture built for high-quality FiveM roleplay.'
-version '0.1.0'
+version '1.0.0'
 
 name 'siku_radio'
 
